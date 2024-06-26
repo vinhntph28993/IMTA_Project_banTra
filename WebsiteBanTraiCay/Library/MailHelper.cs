fa6289274211c4eee96d8d37d86475e6957182b4
vinhntph28993 <tienvinh21022003@gmail.com>
2024-06-26 22:23:39 +0700

    commit_first

---
+++ WebsiteBanTraiCay/Library/MailHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace WebsiteBanTraiCay.Library
+{
+    public class MailHelper
+    {
+        public void SendMail(String to, String subject, String content)
+        {
+            String mailAddress = "testmailaspdotnet@gmail.com";
+            String mailPassword = "elonvirhgtszchvr";
+
+            MailMessage msg = new MailMessage(new MailAddress(mailAddress), new MailAddress(to));
+            msg.Subject = subject;
+            msg.IsBodyHtml = true;
+            msg.Body = content;
+
+            var client = new SmtpClient();
+            client.UseDefaultCredentials = true;
+            client.Credentials = new NetworkCredential(mailAddress, mailPassword);
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Send(msg);
+        }
+    }
+}
