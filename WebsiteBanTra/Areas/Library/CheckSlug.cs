e019b5fc68a649a6af9e6b31413d6292809d8f5f
vinhntph28993 <tienvinh21022003@gmail.com>
2024-06-27 21:26:02 +0700

    Add_Areas_Admin

---
+++ WebsiteBanTra/Areas/Library/CheckSlug.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebsiteBanTraiCay.Models;
+
+namespace WebsiteBanTraiCay
+{
+    public class CheckSlug
+    {
+        ConnectDbContext db = new ConnectDbContext();
+        public bool KiemTraSlug(String Table, String Slug, int? id)
+        {
+            switch (Table)
+            {
+                case "Category":
+                    if (id != null)
+                    {
+                        if (db.Categorys.Where(m => m.Slug == Slug && m.ID != id).Count() > 0)
+                            return false;
+                    }
+                    else
+                    {
+                        if (db.Categorys.Where(m => m.Slug == Slug).Count() > 0)
+                            return false;
+                    }
+                    break;
+                case "Topic":
+                    break;
+                case "Post":
+                    break;
+                case "Product":
+                    break;
+            }
+            return true;
+
+
+        }
+
+    }
+}
