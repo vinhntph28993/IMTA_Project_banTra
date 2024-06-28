1a2a96c71734ed277d717d125d8d6139cc85128c
thanhhuan203 <huan237203@gmail.com>
2024-06-28 21:02:27 +0700

    momo

--- WebsiteBanTra/MomoAPI/PaymentRequest.cs
+++ WebsiteBanTra/MomoAPI/PaymentRequest.cs
@@ -1,64 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Web;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Net;
+// using System.Text;
+// using System.Web;
 
-namespace WebsiteBanTraiCay.MomoAPI
-{
-    public class PaymentRequest
-    {
-        public PaymentRequest()
-        {
-        }
-        public static string sendPaymentRequest(string endpoint, string postJsonString)
-        {
+// namespace WebsiteBanTraiCay.MomoAPI
+// {
+//     public class PaymentRequest
+//     {
+//         public PaymentRequest()
+//         {
+//         }
+//         public static string sendPaymentRequest(string endpoint, string postJsonString)
+//         {
 
-            try
-            {
-                HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create(endpoint);
+//             try
+//             {
+//                 HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create(endpoint);
 
-                var postData = postJsonString;
+//                 var postData = postJsonString;
 
-                var data = Encoding.UTF8.GetBytes(postData);
+//                 var data = Encoding.UTF8.GetBytes(postData);
 
-                httpWReq.ProtocolVersion = HttpVersion.Version11;
-                httpWReq.Method = "POST";
-                httpWReq.ContentType = "application/json";
+//                 httpWReq.ProtocolVersion = HttpVersion.Version11;
+//                 httpWReq.Method = "POST";
+//                 httpWReq.ContentType = "application/json";
 
-                httpWReq.ContentLength = data.Length;
-                httpWReq.ReadWriteTimeout = 30000;
-                httpWReq.Timeout = 15000;
-                Stream stream = httpWReq.GetRequestStream();
-                stream.Write(data, 0, data.Length);
-                stream.Close();
+//                 httpWReq.ContentLength = data.Length;
+//                 httpWReq.ReadWriteTimeout = 30000;
+//                 httpWReq.Timeout = 15000;
+//                 Stream stream = httpWReq.GetRequestStream();
+//                 stream.Write(data, 0, data.Length);
+//                 stream.Close();
 
-                HttpWebResponse response = (HttpWebResponse)httpWReq.GetResponse();
+//                 HttpWebResponse response = (HttpWebResponse)httpWReq.GetResponse();
 
-                string jsonresponse = "";
+//                 string jsonresponse = "";
 
-                using (var reader = new StreamReader(response.GetResponseStream()))
-                {
+//                 using (var reader = new StreamReader(response.GetResponseStream()))
+//                 {
 
-                    string temp = null;
-                    while ((temp = reader.ReadLine()) != null)
-                    {
-                        jsonresponse += temp;
-                    }
-                }
+//                     string temp = null;
+//                     while ((temp = reader.ReadLine()) != null)
+//                     {
+//                         jsonresponse += temp;
+//                     }
+//                 }
 
 
-                //todo parse it
-                return jsonresponse;
-                //return new MomoResponse(mtid, jsonresponse);
+//                 //todo parse it
+//                 return jsonresponse;
+//                 //return new MomoResponse(mtid, jsonresponse);
 
-            }
-            catch (WebException e)
-            {
-                return e.Message;
-            }
-        }
-    }
-}
+//             }
+//             catch (WebException e)
+//             {
+//                 return e.Message;
+//             }
+//         }
+//     }
+// }
